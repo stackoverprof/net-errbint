ec0588fc69cc5cc11300b05391839bd86ac5ae0c
stackoverprof <mail.errbint@gmail.com>
2020-11-21 15:58:32 +0700

    initial commit and deploy

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Styled from '@emotion/styled'
+  
+const Index = () => {
+  return (
+    <Wrapper>
+      <h1>Bismillahirrahmanirrahim</h1>
+    </Wrapper>
+  )
+}
+  
+const Wrapper = Styled.div(() =>`
+  
+`)
+  
+export default Index
