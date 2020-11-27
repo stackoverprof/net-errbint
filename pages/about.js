27fa506f7727968ceef51ffe69f2bccc4395a8d0
stackoverprof <mail.errbint@gmail.com>
2020-11-28 00:11:14 +0700

    firebase succesful usage

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Link from 'next/link'
 import Styled from '@emotion/styled'
+import { DB } from '../services/firebase'
     
 const About = () => {
+
+    useEffect(() => {
+        DB.collection('Leaderboard').orderBy("score.food", "desc").limit(10).onSnapshot(querySnapshot => {
+            var dataSnapshot = []
+            querySnapshot.forEach(doc => {
+                dataSnapshot.push(doc.data().nickname + " " + doc.data().score.food)
+            })
+            console.log("updating leaderboard")
+            console.log(dataSnapshot.join(", "))
+        })
+    }, [])
+
     return (
         <Wrapper>
             <Link href="/"><a className="linktest">home</a></Link>
