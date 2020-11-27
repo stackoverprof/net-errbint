426e76b171686e2539e6448023ba3cfc271611c1
stackoverprof <mail.errbint@gmail.com>
2020-11-28 00:05:55 +0700

    installing firebase

---
+++ services/firebase.js
@@ -0,0 +1,32 @@
+import firebase from 'firebase/app'
+import 'firebase/analytics'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+}
+
+// Initialize Firebase
+const initFirebase = () => {
+    if(!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig)
+        firebase.analytics()
+    }
+}
+
+initFirebase()
+
+const DB = firebase.firestore()
+
+export {
+    DB,
+    initFirebase,
+    firebase as default
+}
