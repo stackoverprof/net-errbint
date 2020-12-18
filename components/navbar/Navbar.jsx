3db6ddcd0db2885d6af15cd063ae2b129d8be013
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2020-12-18 15:27:42 +0700

    navbar on desktop done

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,35 +1,170 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import Styled from '@emotion/styled'
 import Navmenu from './Navmenu'
 
 const Navbar = ({showDrawer}) => {
-
+    const [open, setopen] = useState(false)
+    
     return (
-        <Wrapper>
-            <div className="contain-size">
-                <Navmenu showDrawer={showDrawer}/>
-                <div className="nav-links">
-                    <Link href="/profile">Profile</Link>
-                    <Link href="/abilities">Abilities</Link>
-                    <Link href="/projects">Projects</Link>
-                    <Link href="/experiences">Experiences</Link>
+        <Wrapper open={open} showDrawer={showDrawer}>
+            <div className="navbar">
+                <div className="contain-size">
+                    <Navmenu showDrawer={showDrawer} open={open} setopen={setopen}/>
+                    <div className="nav-links">
+                        <Link href="/">Profile</Link>
+                        <Link href="/abilities">Abilities</Link>
+                        <Link href="/projects">Projects</Link>
+                        <Link href="/experiences">Experiences</Link>
+                    </div>
+                </div>
+            </div>
+            <div className="dropper">
+                <div className="contain-size">
+                    <div className="nav-links">
+                        <Link href="/">Home</Link>
+                        <Link href="/contact">Contact</Link>
+                        <Link href="/about">About</Link>
+                        <Link href="/guestbook">Help</Link>
+                    </div>
+                    <div className="sosmed">
+                        <Link href="https://github.com/stackoverprof/">
+                            <div>
+                                <img src="/img/icons/gh.svg" alt=""/>
+                                <p className="n0">stackoverprof</p>
+                            </div>
+                        </Link>
+                        <Link href="https://instagram.com/errbint">
+                            <div>
+                                <img src="/img/icons/ig.svg" alt=""/>
+                                <p className="n1">@errbint</p>
+                            </div>
+                        </Link>
+                        <Link href="https://line.me/ti/p/FwXuuM_qrV">
+                            <div>
+                                <img src="/img/icons/line.svg" alt=""/>
+                                <p className="n2">@r.bintang11</p>
+                            </div>
+                        </Link>
+                        <Link href="https://wa.me/628988355006">
+                            <div>
+                                <img src="/img/icons/wa.svg" alt=""/>
+                                <p className="n3">+62 8988355006</p>
+                            </div>
+                        </Link>
+                        <Link href="https://www.linkedin.com/in/r-bintang-bagus-putra-angkasa-41a6b1188">
+                            <div>
+                                <img src="/img/icons/lkdn.svg" alt=""/>
+                                <p className="n4">R.Bintang Bagus</p>
+                            </div>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </Wrapper>
     )
 }
 
-const Wrapper = Styled.div(() =>`
+const Wrapper = Styled.div(({open, showDrawer}) =>`
     height: 60px;
     width: 100%;
     background: black;
 
-    position: sticky;
-    top: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    position: relative;
+    top: ${open && !showDrawer ? -60 : 0}px;
+    z-index: 100;
+    transition: 0.25s;
+
+    .sosmed{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        
+        &:hover img{
+            opacity: 0.1;
+        }
+
+        img{
+            transition: 0.25s;
+            width: 24px;
+            height: 24px;
+            pointer-events: none;
+        }
+        
+        div{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transition: 0.25s;
+            cursor: pointer;
+            height: 100%;
+            
+            
+            &:hover img{
+                opacity: 1;
+            }
+            
+            &:hover p.n0{
+                width: 132px;
+            }
+            &:hover p.n1{
+                width: 88px;
+            }
+            &:hover p.n2{
+                width: 120px;
+            }
+            &:hover p.n3{
+                width: 150px;
+            }
+            &:hover p.n4{
+                width: 146px;
+            }
+            &:hover p{
+                color: white;
+            }
+
+            p{
+                white-space: nowrap;
+                font-family: 'Bahnschrift';
+                font-style: normal;
+                font-weight: normal;
+                font-size: 20px;
+                line-height: 24px;
+                text-align: center;
+                margin: 0 10px;
+                color: #555;
+                overflow: hidden;
+                width: 0;
+                transition: 0.25s;
+                text-align: left;
+            }
+
+        }
+    }
+
+    .navbar{
+        width: 100%;
+        height: 60px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .dropper{
+        width: 100%;
+        height: ${open ? 60 : 0}px;
+        background: #181818;
+        overflow: hidden;
+        transition: 0.25s;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        .contain-size{
+            padding-left: 12px;
+        }
+    }
 
     .contain-size{
         max-width: 1192px;
@@ -46,6 +181,7 @@ const Wrapper = Styled.div(() =>`
         justify-content: center;
         align-items: center;
 
+    }
         a{
             font-family: 'Bahnschrift';
             font-style: normal;
@@ -55,8 +191,11 @@ const Wrapper = Styled.div(() =>`
             text-align: center;
             margin: 0 10px;
             color: #FFFFFF;
+
+            &:hover{
+                color: #ff912b;
+            }
         }
-    }
 `)
 
 export default Navbar
--- components/navbar/Navmenu.jsx
+++ components/navbar/Navmenu.jsx
@@ -1,40 +1,136 @@
 import React from 'react'
 import Styled from '@emotion/styled'
 
-const Navmenu = ({showDrawer}) => {
+const Navmenu = ({showDrawer, open, setopen}) => {
 
     return (
-        <Wrapper>
-            <div className="hamburger"></div>
-            <div className="text">
-                <h2 className={`nav ${showDrawer ? 'hiding' : ''}`}>NAVIGATION</h2>
-                <h2 className={`erb ${!showDrawer ? 'hiding' : ''}`}>ERRBINT</h2>
+        <Wrapper onClick={() => setopen(!open)} open={open}>
+            <div className="background"></div>
+            <div className="navmenu">
+                <div className="three col">
+                    <div className={`hamburger ${open ? 'is-active' : ''}`} id="hamburger-10">
+                        <span className="line"></span>
+                        <span className="line"></span>
+                        <span className="line"></span>
+                    </div>
+                </div>
+                <div className="text">
+                    <h2 className={`nav ${showDrawer && open ? '' : showDrawer ? 'sidehide' : ''}`}>NAVIGATION</h2>
+                    <h2 className={`erb ${!showDrawer || open ? 'sidehide' : ''}`}>ERRBINT</h2>
+                </div>
             </div>
         </Wrapper>
     )
 }
 
-const Wrapper = Styled.div(() =>`
+const Wrapper = Styled.div(({open}) =>`
+    position: relative;
     width: 232px;
     height: 100%;
-    background: #3A3A3A;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    padding-left: 20px;
     transition: 0.1s;
     overflow-x: hidden;
-    
-    &:hover{
-        background: #FF5B14;
+
+    .navmenu{
+        position: relative;
+        height: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding-left: 22px;
     }
 
-    .hamburger{
-        height: 28px;
-        width: 28px;
-        background: #fff;
-        margin-right: 12px;
+    .background{
+        position: absolute;
+        background: #222;
+        /* height: ${open ? 0 : 60}px;
+        width: 232px; */
+        height: 60px;
+        width: ${open ? 0 : 232}px;
+        transition: 0.5s;
+        left:0;
     }
+    
+    &:hover .background{
+        background: #111;
+    }
+    
+    .col {
+        display: block;
+        float:left;
+        margin: 1% 0 1% 1.6%;
+    }
+    
+    .col:first-of-type {
+      margin-left: 0;
+    }
+    
+    .three{
+        /* CLEARFIX */
+        
+        .cf:before,
+        .cf:after {
+            content: " ";
+            display: table;
+        }
+        
+        .cf:after {
+            clear: both;
+        }
+        
+        .cf {
+            *zoom: 1;
+        }
+        
+        /* ALL */
+        
+        .row .three{
+          padding: 80px 30px;
+          -webkit-box-sizing: border-box;
+          -moz-box-sizing: border-box;
+          box-sizing: border-box;
+          background-color: #2c3e50;
+          color: #ecf0f1;
+          text-align: center;
+        }
+        
+        .hamburger .line{
+          width: 28px;
+          max-width: 28px;
+          height: 4px;
+          border-radius: 2px;
+          background-color: #ecf0f1;
+          display: block;
+          margin: 6px auto;
+          -webkit-transition: all 0.3s ease-in-out;
+          -o-transition: all 0.3s ease-in-out;
+          transition: all 0.3s ease-in-out;
+        }
+        
+        
+        #hamburger-10{
+            -webkit-transition: all 0.3s ease-in-out;
+            -o-transition: all 0.3s ease-in-out;
+            transition: all 0.3s ease-in-out;
+        }
+        
+        #hamburger-10.is-active{
+            -webkit-transform: rotate(90deg);
+            -ms-transform: rotate(90deg);
+            -o-transform: rotate(90deg);
+            transform: rotate(90deg);
+        }
+        
+        #hamburger-10.is-active .line:nth-of-type(1){
+            width: 7px
+        }
+        
+        #hamburger-10.is-active .line:nth-of-type(2){
+            width: 14px
+        }
+  
+    }
+        
+
 
     .text{
         position: relative;
@@ -42,7 +138,7 @@ const Wrapper = Styled.div(() =>`
         justify-content: center;
         align-items: center;
         height: 100%;
-        width: 100%;
+        width: 172px;
 
         h2{
             position: absolute;
@@ -53,12 +149,17 @@ const Wrapper = Styled.div(() =>`
             font-size: 20pt;
             transition: 2s;
             left: 0;
+            text-align: center;
+            width: 148px;
         }
 
-        .hiding{
+        h2.nav{
+            padding-left: 4px;
+        }
+
+        .sidehide{
             left: 556px;
         }
-        
     }
 `)
 
