0a60dab3cd021e9ee6d4f9fd2c74c79242fa6e9f
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2020-12-28 12:22:25 +0700

    alhamdulillah

--- components/webGL/ColorfulShapes/Canvas.jsx
+++ components/webGL/ColorfulShapes/Canvas.jsx
@@ -1,32 +1,42 @@
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame, useThree, extend } from 'react-three-fiber/web.cjs'
 import { useSpring, a } from 'react-spring/three.cjs'
+import { softShadows } from 'drei/softShadows.cjs'
 import './setupRegTHREE'
 import 'three/examples/js/controls/OrbitControls'
 
-extend({ OrbitControls: THREE.OrbitControls });
+extend({ OrbitControls: THREE.OrbitControls })
+softShadows({
+  frustrum: 3.75, // Frustrum width (default: 3.75)
+  size: 0.01, // World size (default: 0.005)
+  near: 9.5, // Near plane (default: 9.5)
+  samples: 27, // Samples (default: 17)
+  rings: 11, // Rings (default: 11)
+})
 
-const Box = (props) => {
+const Box = ({args, position}) => {
     const mesh = useRef()
     const [hovered, setHover] = useState(false)
     
     useFrame(() => {
-      mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+      mesh.current.rotation.x += 0.01
+      mesh.current.rotation.y += 0.01
     })
 
     const springs = useSpring({
-        scale: hovered ? [1.4, 1.4, 1.4] : [1, 1, 1],
-        color: hovered ? '#FF5B14' : '#DADADA'
+        scale: hovered ? [1.15, 1.15, 1.15] : [1, 1, 1],
+        color: hovered ? '#FF5B14' : '#555555'
     })
     
     return (
       <a.mesh
-        {...props}
+        position={position}
         ref={mesh}
         scale={springs.scale}
+        castShadow
         onPointerOver={() => setHover(true)}
         onPointerOut={() => setHover(false)}>
-        <boxBufferGeometry args={[1, 1, 1]} />
+        <boxBufferGeometry args={args} />
         <a.meshStandardMaterial color={springs.color} />
       </a.mesh>
     )
@@ -50,8 +60,14 @@ const Scene = ({children, mouse}) => {
 
     return (
       <>
+        <color attach="background" args={['#000']} />
+        <fog attach="fog" near={0} far={30} color="#000" />
+        <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]}>
+          <planeBufferGeometry attach="geometry" args={[1000, 1000]}/>
+          <meshStandardMaterial attach="material" color="#333" />
+        </mesh>
         {children}
-        <orbitControls args={[camera, domElement]} enableZoom={false}/>
+        <orbitControls args={[camera, domElement]} enableZoom={false} enablePan={false} enableKey={false}/>
         <Rig mouse={mouse} />
     </>
     )
@@ -68,11 +84,26 @@ const CanvasApp = () => {
         camera={{ position: [0, 0, 10], fov: 25 }} 
         onMouseMove={e => (mouse.current = [e.clientX - window.innerWidth / 2, e.clientY - window.innerHeight / 2])}>
         <Scene mouse={mouse}>
-            <ambientLight intensity={0.5} />
-            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-            <pointLight position={[-10, -10, -10]} />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
+          <ambientLight intensity={0.3}/>
+          <directionalLight 
+              castShadow
+              position={[0, 10, 0]} 
+              intensity={0.5} 
+              shadow-mapSize-width={1024}
+              shadow-mapSize-height={1024}
+              shadow-camera-far={50}
+              shadow-camera-left={-10}
+              shadow-camera-right={10}
+              shadow-camera-top={10}
+              shadow-camera-bottom={-10}
+
+          />
+          <pointLight position={[-10, 0, -20]} intensity={0.5}/>
+          <pointLight position={[0, -10, 0]} intensity={1.5}/>
+
+          <Box args={[1, 1, 1]} position={[-4, 0, 0]} />
+          <Box args={[1, 1, 1]} position={[4, 0, 0]} />
+          <Box args={[0.05, 0.05, 0.05]} position={[0, 0, 0]} />
         </Scene>
       </Canvas>
   </>
