0bdd200fee6a87956ddb90181198cd5736974b11
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-01-01 14:29:58 +0700

    cleaning code

--- components/webGL/ColorfulShapes/Canvas.jsx
+++ components/webGL/ColorfulShapes/Canvas.jsx
@@ -3,6 +3,7 @@ import { Canvas, useFrame, useThree, extend } from 'react-three-fiber/web.cjs'
 import { useSpring, a } from 'react-spring/three.cjs'
 import { softShadows } from 'drei/softShadows.cjs'
 import './setupRegTHREE'
+import Rig from './Rig'
 import 'three/examples/js/controls/OrbitControls'
 import useMouse from './useMouse'
 import useResize from "use-resizing"
@@ -45,51 +46,53 @@ const Box = ({args, pos, rotate, canvasHover}) => {
 
       <boxBufferGeometry args={args} />
       <a.meshStandardMaterial color={springs.color} />
-
-      {/* {canvasHover && !hovered ?
-        <meshNormalMaterial attach="material"/>
-      : 
-        <a.meshStandardMaterial color={springs.color}/>
-      } */}
-
     </a.mesh>
   )
 }
 
-const FlashLight = ({canvasHover}) => {
-  const mesh = useRef(null)
-
-  const springs = useSpring({
-    intensity: !canvasHover ? 2 : 0
-  })
-
-  return (
-    <a.pointLight ref={mesh} castShadow position={[0, -1, 0]} intensity={springs.intensity} color="white"/>
-  )
-}
-
-const Rig = ({ mouse }) => {
-  const { camera } = useThree()
-  useFrame(() => {
-    camera.position.x += (mouse.current[0] / 50 - camera.position.x) * 0.05
-    camera.position.y += (-mouse.current[1] / 50 - camera.position.y) * 0.05
-    camera.lookAt(0, 0, 0)
-  })
-  return null
-}
-
-const Scene = ({children, mouse, touchDevice}) => {  
+const Scene = ({children, mouse, touchDevice, canvasHover}) => {  
     const { camera, gl } = useThree()
+
+    const springs = useSpring({
+      intensity: !canvasHover ? 2 : 0
+    })
+
+    const Lighting = () => {
+      return (
+      <>  
+        <ambientLight intensity={0.3}/>
+        <a.pointLight castShadow position={[0, -1, 0]} intensity={springs.intensity} color="white"/>
+        <pointLight position={[-10, 0, -20]} intensity={0.5}/>
+        <pointLight position={[0, -10, 0]} intensity={1.5}/>
+        <directionalLight 
+          castShadow
+          position={[0, 10, 0]} 
+          intensity={0.5} 
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+          />
+      </>
+      )
+    }
 
     return (
       <>
-        <color attach="background" args={['#000']} />
         <fog attach="fog" near={0} far={30} color="#000" />
+        <color attach="background" args={['#000']} />
+        <Lighting />
+
+        { children }
+
         <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]}>
           <planeBufferGeometry attach="geometry" args={[1000, 1000]}/>
           <meshStandardMaterial attach="material" color="#333" />
         </mesh>
-        { children }
+
         {touchDevice ? 
           <orbitControls args={[camera, gl.domElement]} enableZoom={false} enablePan={false} enableKey={false}/>
         :
@@ -106,51 +109,23 @@ const Scene = ({children, mouse, touchDevice}) => {
 
     const screen = useResize().width
     const adjustWide = screen < 600 ? 600/1300 : screen/1300
-
-    const [lightPos, setlightPos] = useState({x: 0, y: 0})
-    // const [canvasHover, setcanvasHover] = useState(false)
     
     return (
-      <>
-      <Canvas 
-        shadowMap 
-        colorManagement 
-        // onPointerOver={() => setcanvasHover(!drawerTransition)}
-        // onPointerOut={() => setcanvasHover(false)}
-        onMouseMove={e => setlightPos({x: e.clientX - window.innerWidth/2, y: e.clientY - 60 -348/2})}
-        camera={{ position: [0, 0, 10], fov: 25 }}>
-        <Scene mouse={mouse} touchDevice={touchDevice}>
-          <ambientLight intensity={0.3}/>
-          <directionalLight 
-              castShadow
-              position={[0, 10, 0]} 
-              intensity={0.5} 
-              shadow-mapSize-width={1024}
-              shadow-mapSize-height={1024}
-              shadow-camera-far={50}
-              shadow-camera-left={-10}
-              shadow-camera-right={10}
-              shadow-camera-top={10}
-              shadow-camera-bottom={-10}
-
-          />
-          <pointLight position={[-10, 0, -20]} intensity={0.5}/>
-          <pointLight position={[0, -10, 0]} intensity={1.5}/>
-          
-          <FlashLight lightPos={lightPos} canvasHover={canvasHover}/>
-
-          <Box pos={[adjustWide*-5, 1, 0]} rotate={10} args={[1, 1, 1]} canvasHover={canvasHover}/>
-          <Box pos={[adjustWide*-5.5, -1, -3]} rotate={40} args={[1, 1, 1]} canvasHover={canvasHover}/>
-          <Box pos={[adjustWide*-5, 1, 4]} rotate={110} args={[1, 1, 1]} canvasHover={canvasHover}/>
-
-          <Box pos={[adjustWide*5, 0, 0]} rotate={30} args={[1, 1, 1]} canvasHover={canvasHover}/>
-          <Box pos={[adjustWide*6, 2, 0]} rotate={60} args={[1, 1, 1]} canvasHover={canvasHover}/>
-          <Box pos={[adjustWide*4, 0, 5]} rotate={110} args={[1, 1, 1]} canvasHover={canvasHover}/>
-          {/* <Box args={[0.05, 0.05, 0.05]} position={[0, 0, 0]} /> */}
+      <Canvas shadowMap camera={{ position: [0, 0, 10], fov: 25 }}>
+        <Scene mouse={mouse} touchDevice={touchDevice} canvasHover={canvasHover}>
+          <group>
+            <Box pos={[adjustWide*-5, 1, 0]} rotate={10} args={[1, 1, 1]} canvasHover={canvasHover}/>
+            <Box pos={[adjustWide*-5.5, -1, -3]} rotate={40} args={[1, 1, 1]} canvasHover={canvasHover}/>
+            <Box pos={[adjustWide*-5, 1, 4]} rotate={110} args={[1, 1, 1]} canvasHover={canvasHover}/>
+          </group>
+          <group>
+            <Box pos={[adjustWide*5, 0, 0]} rotate={30} args={[1, 1, 1]} canvasHover={canvasHover}/>
+            <Box pos={[adjustWide*6, 2, 0]} rotate={60} args={[1, 1, 1]} canvasHover={canvasHover}/>
+            <Box pos={[adjustWide*4, 0, 5]} rotate={110} args={[1, 1, 1]} canvasHover={canvasHover}/>
+          </group>
         </Scene>
       </Canvas>
-  </>
-  )
+    )
 }
   
   export default CanvasApp
