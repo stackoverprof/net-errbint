72bc19d06aa33a47d08024f4aff7fb488290a163
errbint <mail.errbint@gmail.com>
2021-06-18 00:38:43 +0700

    Give time for intro

--- components/game/Rainbox/Canvas.jsx
+++ components/game/Rainbox/Canvas.jsx
@@ -435,7 +435,7 @@ const Canvas = ({_initialLoad, skipIntro, setanimateValue, setprocessMessage, se
         let shapes = {}
         let food = {}
         let executeGame = false
-        let delay = 0
+        let delay = 1000
         
         const IgniteGame = () => {
             setgameStatus('initial')
