9655a4b1c17b15ca7aacb1cc4323d2b910509ebf
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-08-15 05:21:32 +0700

    feat: theme config

---
+++ components/Game/Rainbox/theme.ts
@@ -0,0 +1,22 @@
+export const GameTheme = {
+	orange: {
+		dark: '#ff4a14',
+		light: '#ff9914',
+		shadow: '#FFA500',
+	},
+	purple: {
+		dark: '#7810ff',
+		light: '#c42eff',
+		shadow: '#8c00ff',
+	},
+	green: {
+		dark: '#45cf22',
+		light: '#a3e20f',
+		shadow: '#91ff00',
+	},
+	blue: {
+		dark: '#0d78db',
+		light: '#10bbff',
+		shadow: '#00a2ff',
+	},
+};
