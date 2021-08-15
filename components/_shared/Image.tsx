490caaabdc531915889b3761b822bad074c5295e
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-08-15 20:25:30 +0700

    feat: Added Custom NextImage

---
+++ components/_shared/Image.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import NextImage, { ImageLoader } from 'next/image';
+
+interface Props {
+	src: string
+	alt: string
+	ref: React.MutableRefObject<HTMLDivElement>
+	className?: string
+	style?: React.CSSProperties
+	blurDataURL?: never
+	loader?: ImageLoader
+	quality?: number | string
+	width?: number | string
+	height?: number | string
+	priority?: boolean
+	loading?: 'lazy' | 'eager'
+	unoptimized?: boolean
+	objectFit?: 'fill' | 'contain' | 'cover' | 'none' | 'scale-down'
+	objectPosition?: string | 'inherit' | 'initial' | 'unset'	
+}
+
+const Image = ({src, alt, ref, className, style, objectFit, blurDataURL, loader, quality, priority, loading, unoptimized, objectPosition, width, height}: Props): JSX.Element => (
+	<div ref={ref} className={`relative ${className}`} style={{...style, width, height}}>
+		<NextImage
+			objectFit={objectFit}
+			layout="fill"
+			src={src}
+			alt={alt}
+			blurDataURL={blurDataURL}
+			loader={loader}
+			quality={quality}
+			priority={priority}
+			loading={loading}
+			unoptimized={unoptimized}
+			objectPosition={objectPosition}
+		/>
+	</div>
+);
+
+export default Image;
