d3853b4ad5aa28a7208b20ec176014b6dde67d7e
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-08-21 06:46:43 +0700

    404 and abouty update

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,15 @@
-import Link from '@components/_shared/Link';
 import React from 'react';
+import Link from '@components/_shared/Link';
 
-const error404 = (): JSX.Element => {
+const Page404 = (): JSX.Element => {
 
 	return (
 		<div className="fullscreen flex-cc col">
 			<h1 className="mb-4">404 | Not Found</h1>
-			<Link href="/"><button className="px-4 py-2  text-black bg-white">BACK HOME</button></Link>
+			<Link href="/" className="px-4 py-2  text-black bg-white">BACK HOME</Link>
 		</div>
 	);
 };
 
 
-export default error404;
+export default Page404;
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import Link from '@components/_shared/Link';
 import MainLayout from '@components/_layouts/MainLayout';
-
+import { useLayout } from '@core/contexts';
+import Link from '@components/_shared/Link';
+ 
 const About = (): JSX.Element => {
-
+	const { Alert } = useLayout(); 
+	
 	return (
-		<MainLayout title="About" className="flex-sc col" transition="fade">
-			<h1 className="font-bold text-3xl font-bahn">About</h1>
-			<Link href="/" className="px-4 py-2 text-base bg-primary" stroked>home</Link>
+		<MainLayout title="About" className="flex-sc col">
+			<p className="z-10 max-w-sm mt-48 mb-2 text-center">The technologies used in this template</p>
+			<div className="flex-cc gap-1 mb-4">
+				{/* eslint-disable-next-line @next/next/no-img-element */}
+				<img src="https://img.shields.io/badge/NEXT%20-%23000000.svg?&style=flat&logo=next.js&logoColor=white" alt="tech" />
+				{/* eslint-disable-next-line @next/next/no-img-element */}
+				<img src="https://img.shields.io/badge/TAILWIND%20-%2338B2AC.svg?&style=flat&logo=tailwindcss&logoColor=white" alt="tech" />
+			</div>
+			<p className="z-10 max-w-sm mb-16 text-center"><strong>Extras:</strong> Layout, Alert, Custom Link, Custom Image, SEOTags, useForm, Context setup</p>
+			
+			<div className="flex-cc gap-4 mb-24">
+				<Link href="/" className="px-4 py-2 text-white bg-accent hover:bg-opacity-80">BACK HOME</Link>
+				<button 
+					type="submit"
+					onClick={() => Alert({message: 'More variation of color with the props type: something', type: 'success'})}
+					className="px-4 py-2 text-white bg-black hover:bg-opacity-80"
+				>
+					Try Alert
+				</button>
+			</div>
 		</MainLayout>
 	);
 };
-
 
 export default About;
