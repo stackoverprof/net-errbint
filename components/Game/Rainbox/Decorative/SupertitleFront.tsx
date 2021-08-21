94bed9091e3ecd1d97313495ca42bc2d71e1c9a6
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-08-21 19:16:37 +0700

    fix: supertitle front to img

--- components/Game/Rainbox/Decorative/SupertitleFront.tsx
+++ components/Game/Rainbox/Decorative/SupertitleFront.tsx
@@ -17,7 +17,19 @@ const SupertitleFront = ({ skipIntro }: Props) => {
 	return (
 		<div className="fixed inset-0 z-0 flex-cc pb-40 pointer-events-none full" style={{opacity: (screen_height/2 - scroll ) / (screen_height/2)}}>
 			<div className="absolute flex-cc col w-full">
-				<div className={'transition-all bg-center bg-contain bg-no-repeat opacity-100'} style={{backgroundImage: 'url("/img/title/h1.svg")', maxWidth: 675, width: '90%',minWidth: 340, height: 200, transitionDuration: gameStatus === 'sub_intro' ? '2.5s' : '1s', opacity: { 'intro': '0', 'sub_intro': '1', 'ready': '1', 'over': '1', 'running': '0'}[gameStatus] || '1'}}></div>
+				<img 
+					src="/img/title/h1.svg" 
+					alt="" 
+					className="transition-all object-center object-contain opacity-100" 
+					style={{ 
+						maxWidth: 675, 
+						width: '90%', 
+						minWidth: 340, 
+						height: 200, 
+						transitionDuration: gameStatus === 'sub_intro' ? '2.5s' : '1s', 
+						opacity: { intro: '0', sub_intro: '1', ready: '1', over: '1', running: '0'}[gameStatus] || '1'
+					}}
+				/>
 				<div className="relative flex-cc transition-all" style={{zIndex: -1, top: screen > 500 ? '-12px' : '-48px', transition: skipIntro ? 'none' : gameStatus === 'running' || gameStatus === 'over' ? 'all 1s 2s, opacity 1s' : 'all 1s 2s', opacity: {intro: '0', running: '0'}[gameStatus] || '1', minHeight: '38px'}}>
 					<p className="pt-1 text-center transition-all" style={{fontSize: screen > 500 ? 32 : 24, color: gameStatus == 'running' ? '#BBBBBB' :	gameStatus == 'over' ? 'black' : 'gray'}}>
 						{{
