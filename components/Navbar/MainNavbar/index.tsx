573ef658e826603ab7b0e96ca181a9c1a81ae4e4
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-08-30 00:33:10 +0700

    feat: added navmenu

--- components/Navbar/MainNavbar/index.tsx
+++ components/Navbar/MainNavbar/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
+import NavMenu from './NavMenu';
 
 const MainNavbar = () => {
 	return (
 		<nav className="sticky top-0 bg-red-200 w-full">
 			<Navbar />
+			<NavMenu />
 		</nav>
 	);
 };
