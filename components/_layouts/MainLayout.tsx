4c67c6480c12993b49019070c31009f80fa21e50
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-08-29 19:13:06 +0700

    feat: scrollUp option in MainLayout

--- components/_layouts/MainLayout.tsx
+++ components/_layouts/MainLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AlertHandler from '@components/_shared/AlertHandler';
 import { useLayout } from '@core/contexts/index';
 import useClearance from '@core/hooks/useClearance';
@@ -10,12 +10,17 @@ interface Props {
 	title?: string
 	className?: string
 	style?: React.CSSProperties
-	transition?: 'fade' | '';
+	transition?: 'fade' | ''
+	scrollUp?: boolean
 }
 
-const MainLayout = ({children, title, className, transition, style}: Props): JSX.Element => {
+const MainLayout = ({children, title, className, transition, style, scrollUp}: Props): JSX.Element => {
 	const { alert_value, resetAlert } = useLayout();
 	const [clearance, upperRef, lowerRef] = useClearance();
+
+	useEffect(() => {
+		if (scrollUp) window.scrollTo(0, window.innerHeight * 4/5);
+	}, []);
 
 	return (
 		<Transition type={transition}>
