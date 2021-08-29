3cb4f870189491db12a065ce9c450fb3c71d180e
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-08-30 06:54:29 +0700

    feat: menu opening behavior

--- components/Navbar/MainNavbar/Navbar.tsx
+++ components/Navbar/MainNavbar/Navbar.tsx
@@ -1,12 +1,19 @@
 import Link from '@components/_shared/Link';
 import React from 'react';
 
-const Navbar = () => {
+interface Props {
+	isActive: boolean
+	toggleOpenMenu(): void;
+}
+
+const Navbar = ({isActive, toggleOpenMenu}: Props) => {
 
 	return (
 		<div className="w-full bg-blue-300 flex-cc" style={{height: 60}}>
 			<div className="container-14 flex-bc">
-				<div>haha</div>
+				<div className="h-full w-40">
+					<button onClick={() => toggleOpenMenu()}>NAVIGATION {isActive ? 'active' : 'close'}</button>
+				</div>
 
 				<div className="flex-cc gap-2">
 					<Link href="/">HOME</Link>
--- components/Navbar/MainNavbar/index.tsx
+++ components/Navbar/MainNavbar/index.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useScrollYPosition } from 'react-use-scroll-position';
-import useDebug from '@core/hooks/useDebug';
 import Navbar from './Navbar';
 import NavMenu from './NavMenu';
 import useResize from 'use-resizing';
@@ -10,17 +9,31 @@ const MainNavbar = () => {
 	const [openMenu, setOpenMenu] = useState(false);
 	const scroll = useScrollYPosition();
 
+	const shrinkRef = useRef(null);
+
 	const screenHeight = useResize().height;
-	
+
+	const _setOpenMenu = () => {
+		shrinkRef.current.style.transition = 'all 0.15s ease 0s';
+		setOpenMenu(!openMenu);
+	};
 	
 	const getShrink = () => {
+		if (shrinkRef.current) {
+			const removeTransition = () => shrinkRef.current.style.transition = 'none';
+			if (openMenu) removeTransition();
+			else setTimeout(removeTransition, 150);
+		}
 		const calc = 60 - (scroll - (screenHeight - 60)); 
 		const shrink = calc < 0 ? 0 : calc > 60 ? 60 : calc;
 		return shrink;
 	};
 
 	useEffect(() => {			
-		if (scroll === 0) setPosition('bottom');
+		if (scroll === 0) {
+			setPosition('bottom');
+			setOpenMenu(false);
+		}
 		else if (scroll > window.innerHeight - 60) setPosition('top');
 		else setPosition('floating');
 	}, [scroll]);
@@ -28,9 +41,9 @@ const MainNavbar = () => {
 	return (
 		<nav className="sticky top-0 w-full pointer-events-none" style={{height: 120}}>
 			<div style={{height: 60}} className="pointer-events-auto">
-				<Navbar />
+				<Navbar isActive={position !== 'bottom' && (scroll < screenHeight || openMenu)} toggleOpenMenu={_setOpenMenu} />
 			</div>
-			<div style={{height: getShrink()}} className="pointer-events-auto overflow-hidden flex-cc">
+			<div ref={shrinkRef} style={{height: openMenu ? 60 : getShrink()}} className="transition-all pointer-events-auto overflow-hidden flex-cc">
 				<NavMenu />
 			</div>
 		</nav>
