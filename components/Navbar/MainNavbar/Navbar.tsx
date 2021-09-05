aa41bae2ba493d576f7b8ca634a9ffd662e0f1a7
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-09-06 00:50:02 +0700

    feat: shadow scroller

--- components/Navbar/MainNavbar/Navbar.tsx
+++ components/Navbar/MainNavbar/Navbar.tsx
@@ -57,10 +57,10 @@ const PageLinks = () => {
 	};
 
 	const theme_switch_text = {
-		orange: 'text-accent-orange',
-		purple: 'text-accent-purple',
-		green: 'text-accent-green',
-		blue: 'text-accent-blue',
+		orange: '-sm:text-accent-orange',
+		purple: '-sm:text-accent-purple',
+		green: '-sm:text-accent-green',
+		blue: '-sm:text-accent-blue',
 	};
 
 	useEffect(() => {
@@ -68,17 +68,20 @@ const PageLinks = () => {
 	}, [router]);
 	
 	return (
-		<div className="relative flex-cc -sm:flex-sc h-full text-xl text-white group -sm:overflow-x-scroll" onMouseEnter={() => setInside(true)} onMouseLeave={() => { setInside(false); setHovered(currentPage); }}>
-			<div className="z-10 flex-cc h-full gap-2 -sm:px-6">
-				{data_links.map((item, i) => (
-					<div ref={childRef[i]} key={i} className="h-full">
-						<Link href={item.route} scroll={false} onMouseEnter={() => setHovered(i)} className={`flex-cc h-full px-3 ${router.pathname === item.route ? theme_switch_text[selectedTheme] : ''}`}>{item.text}</Link>
-					</div>
-				))}
-			</div>
+		<>			
+			<div className="relative flex-cc -sm:flex-sc h-full text-xl text-white group -sm:overflow-x-scroll -sm:no-scrollbar" onMouseEnter={() => setInside(true)} onMouseLeave={() => { setInside(false); setHovered(currentPage); }}>
+				<div className="z-10 flex-cc h-full gap-2 -sm:px-6">
+					{data_links.map((item, i) => (
+						<div ref={childRef[i]} key={i} className="h-full">
+							<Link href={item.route} scroll={false} onMouseEnter={() => setHovered(i)} className={`flex-cc h-full px-3 ${router.pathname === item.route ? theme_switch_text[selectedTheme] : ''}`}>{item.text}</Link>
+						</div>
+					))}
+				</div>
 
-			<div className={['absolute -sm:hidden h-8 transition-all', theme_switch[selectedTheme]].join(' ')} style={{borderRadius: 6, opacity: inside ? 0.5 : 0.9, left: getPrevDistance(inside ? hovered : currentPage) + (8*hovered), transitionDuration: '0.25s', width: childRef[hovered].current?.offsetWidth || 0}}></div>
-		</div>
+				<div className={['absolute -sm:hidden h-8 transition-all', theme_switch[selectedTheme]].join(' ')} style={{borderRadius: 6, opacity: inside ? 0.5 : 0.9, left: getPrevDistance(inside ? hovered : currentPage) + (8*hovered), transitionDuration: '0.25s', width: childRef[hovered].current?.offsetWidth || 0}}></div>
+			</div>
+			<div className="absolute w-32 sm:hidden right-0 pointer-events-none z-10" style={{height: 60, background: 'linear-gradient(90deg, #0000 0%, #000F 100%)'}}></div>
+		</>
 	);
 };
 
