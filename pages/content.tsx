c4a6babe4e7ef9dda13a4692f15bc25ff682e63a
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-09-05 15:49:07 +0700

    feat: sample pages

--- pages/content.tsx
+++ pages/content.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import MainLayout from '@components/_layouts/MainLayout';
 
-const Projects = (): JSX.Element => {
+const Content = (): JSX.Element => {
 	return (
 		<MainLayout title="About" className="flex-sc col" transition="fade" scrollUp>
 			<div className="w-full bg-black text-white" style={{height: 2000}}>
 				<div className="flex-cs pt-24 gap-2 h-full">
-					INI Projects				
+					INI Content		Blog dan Solution		
 				</div>
 			</div>
 		</MainLayout>
 	);
 };
 
-export default Projects;
+export default Content;
--- pages/gears.tsx
+++ pages/gears.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import MainLayout from '@components/_layouts/MainLayout';
-import Link from '@components/_shared/Link';
- 
-const About = (): JSX.Element => {
+
+const Gears = (): JSX.Element => {
 	return (
 		<MainLayout title="About" className="flex-sc col" transition="fade" scrollUp>
 			<div className="w-full bg-black text-white" style={{height: 2000}}>
-				<div className="flex-sc gap-2">
-					<Link href="/">HOME</Link>
-					<Link href="/about">ABOUT</Link>
-					<Link href="/projects">PROJECTS</Link>
+				<div className="flex-cs pt-24 gap-2 h-full">
+					INI Gears				
 				</div>
 			</div>
 		</MainLayout>
 	);
 };
 
-export default About;
+export default Gears;
